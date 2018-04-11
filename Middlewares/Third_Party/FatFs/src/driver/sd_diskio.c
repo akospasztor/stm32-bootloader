5238c483b7daead0b506fcd3ae4e07cee9bd6d34
Akos Pasztor <mail@akospasztor.com>
2018-04-11 16:47:47 +0200

    hal update

--- Middlewares/Third_Party/FatFs/src/driver/sd_diskio.c
+++ Middlewares/Third_Party/FatFs/src/driver/sd_diskio.c
@@ -1,104 +1,91 @@
 /**
   ******************************************************************************
-  * @file    sd_diskio.c (based on sd_diskio_template.c v2.0.1)
-  * @brief   SD Disk I/O driver
+  * STM32L4 Bootloader
   ******************************************************************************
-  * This notice applies to any and all portions of this file
-  * that are not between comment pairs USER CODE BEGIN and
-  * USER CODE END. Other portions of this file, whether 
-  * inserted by the user or by software development tools
-  * are owned by their respective copyright owners.
-  *
-  * Copyright (c) 2017 STMicroelectronics International N.V. 
-  * All rights reserved.
-  *
-  * Redistribution and use in source and binary forms, with or without 
-  * modification, are permitted, provided that the following conditions are met:
-  *
-  * 1. Redistribution of source code must retain the above copyright notice, 
-  *    this list of conditions and the following disclaimer.
-  * 2. Redistributions in binary form must reproduce the above copyright notice,
-  *    this list of conditions and the following disclaimer in the documentation
-  *    and/or other materials provided with the distribution.
-  * 3. Neither the name of STMicroelectronics nor the names of other 
-  *    contributors to this software may be used to endorse or promote products 
-  *    derived from this software without specific written permission.
-  * 4. This software, including modifications and/or derivative works of this 
-  *    software, must execute solely and exclusively on microcontroller or
-  *    microprocessor devices manufactured by or for STMicroelectronics.
-  * 5. Redistribution and use of this software other than as permitted under 
-  *    this license is void and will automatically terminate your rights under 
-  *    this license. 
-  *
-  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS" 
-  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT 
-  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
-  * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
-  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT 
-  * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
-  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
-  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
-  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  * @author Akos Pasztor
+  * @file   sd_diskio.c
+  * @brief  SD diskio driver
+  *	        This file contains the implementation of the SD diskio driver
+  *         used by the FatFs module. The driver uses the HAL library of ST.
   *
   ******************************************************************************
-  */ 
-/* USER CODE BEGIN firstSection */
-/* can be used to modify / undefine following code or add new definitions */
-/* USER CODE END firstSection*/
+  * Copyright (c) 2018 Akos Pasztor.                    https://akospasztor.com
+  ******************************************************************************
+**/
 
-/* Includes ------------------------------------------------------------------*/
 #include "ff_gen_drv.h"
 #include "sd_diskio.h"
 
-/* Private typedef -----------------------------------------------------------*/
-/* Private define ------------------------------------------------------------*/
+/* Defines -------------------------------------------------------------------*/
+#define SD_TIMEOUT      SD_DATATIMEOUT      /* Defined in bsp_driver_sd.h */
+
+/*
+ * Depending on the use case, the SD card initialization could be done at the
+ * application level: if it is the case, disable the define below to disable
+ * the BSP_SD_Init() call in the SD_Initialize() and manually add a call to 
+ * BSP_SD_Init() elsewhere in the application.
+ */
+//#define ENABLE_SD_INIT
+     
+/* Enable the define below to use the SD driver with DMA.
+ * The define has effect in SD_read and SD_write.
+ * BSP_DRIVER_SD should handle the DMA MSP initialization.
+ */
+#define ENABLE_SD_DMA_DRIVER
+
+/* 
+ * When using cachable memory region, it may be needed to maintain the cache
+ * validity. Enable the define below to activate a cache maintenance at each
+ * read and write operation.
+ * Notice: This is applicable only for cortex M7 based platform.
+ */
+//#define ENABLE_SD_DMA_CACHE_MAINTENANCE 1
+
 /* Private variables ---------------------------------------------------------*/
-/* Disk status */
-static volatile DSTATUS Stat = STA_NOINIT;
+static volatile DSTATUS Stat = STA_NOINIT;      /* Disk status */
+static volatile UINT    ReadStatus = 0;
+static volatile UINT    WriteStatus = 0;
+
 /* Private function prototypes -----------------------------------------------*/
 static DSTATUS SD_CheckStatus(BYTE lun);
 DSTATUS SD_initialize (BYTE);
 DSTATUS SD_status (BYTE);
 DRESULT SD_read (BYTE, BYTE*, DWORD, UINT);
+
 #if _USE_WRITE == 1
-  DRESULT SD_write (BYTE, const BYTE*, DWORD, UINT);
+DRESULT SD_write (BYTE, const BYTE*, DWORD, UINT);
 #endif /* _USE_WRITE == 1 */
+  
 #if _USE_IOCTL == 1
-  DRESULT SD_ioctl (BYTE, BYTE, void*);
+DRESULT SD_ioctl (BYTE, BYTE, void*);
 #endif  /* _USE_IOCTL == 1 */
+
 
 const Diskio_drvTypeDef  SD_Driver =
 {
-  SD_initialize,
-  SD_status,
-  SD_read,
+    SD_initialize,
+    SD_status,
+    SD_read,
 #if  _USE_WRITE == 1
-  SD_write,
+    SD_write,
 #endif /* _USE_WRITE == 1 */
-
 #if  _USE_IOCTL == 1
-  SD_ioctl,
+    SD_ioctl,
 #endif /* _USE_IOCTL == 1 */
 };
 
-/* USER CODE BEGIN beforeFunctionSection */
-/* can be used to modify / undefine following code or add new code */
-/* USER CODE END beforeFunctionSection */
 
 /* Private functions ---------------------------------------------------------*/
 static DSTATUS SD_CheckStatus(BYTE lun)
 {
-  Stat = STA_NOINIT;
+    Stat = STA_NOINIT;
 
-  if(BSP_SD_GetCardState() == MSD_OK)
-  {
-    Stat &= ~STA_NOINIT;
-  }
+    if(BSP_SD_GetCardState() == MSD_OK)
+    {
+        Stat &= ~STA_NOINIT;
+    }
 
-  return Stat;
+    return Stat;
 }
 
 /**
@@ -108,7 +95,21 @@ static DSTATUS SD_CheckStatus(BYTE lun)
   */
 DSTATUS SD_initialize(BYTE lun)
 {
-  return SD_CheckStatus(lun);
+    Stat = STA_NOINIT;
+    
+#if defined(ENABLE_SD_INIT)
+    if(BSP_SD_Init() == MSD_OK)
+    {
+        Stat = SD_CheckStatus(lun);
+    }
+#else
+    Stat = SD_CheckStatus(lun);
+#endif
+    
+    ReadStatus = 0;
+    WriteStatus = 0;
+    
+    return Stat;
 }
 
 /**
@@ -118,12 +119,9 @@ DSTATUS SD_initialize(BYTE lun)
   */
 DSTATUS SD_status(BYTE lun)
 {
-  return SD_CheckStatus(lun);
+    return SD_CheckStatus(lun);
 }
 
-/* USER CODE BEGIN beforeReadSection */
-/* can be used to modify previous code / undefine following code / add new code */
-/* USER CODE END beforeReadSection */
 /**
   * @brief  Reads Sector(s)
   * @param  lun : not used
@@ -134,25 +132,79 @@ DSTATUS SD_status(BYTE lun)
   */
 DRESULT SD_read(BYTE lun, BYTE *buff, DWORD sector, UINT count)
 {
-  DRESULT res = RES_ERROR;
-
-  if(BSP_SD_ReadBlocks((uint32_t*)buff,
-                       (uint32_t) (sector),
-                       count, SDMMC_DATATIMEOUT) == MSD_OK)
-  {
-    /* wait until the read operation is finished */
-    while(BSP_SD_GetCardState()!= MSD_OK)
+    DRESULT  res;
+    uint32_t timeout;
+    
+#if defined(ENABLE_SD_DMA_DRIVER)
+    /* Use SD Driver in DMA mode */
+    
+#if defined(ENABLE_SD_DMA_CACHE_MAINTENANCE)
+    uint32_t alignedAddr;
+#endif /* SD_DMA_CACHE_MAINTENANCE */
+    
+    res = RES_ERROR;
+    ReadStatus = 0;
+    
+    if(BSP_SD_ReadBlocks_DMA((uint32_t*)buff, (uint32_t)(sector), count) == MSD_OK)
     {
+        /* Wait for DMA Complete */
+        timeout = HAL_GetTick();
+        while((ReadStatus == 0) && ((HAL_GetTick() - timeout) < SD_TIMEOUT))
+        {
+        }
+        
+        /* In case of a timeout return error */
+        if(ReadStatus == 0)
+        {
+            res = RES_ERROR;
+        }
+        else
+        {
+            ReadStatus = 0;
+            timeout = HAL_GetTick();
+            while((HAL_GetTick() - timeout) < SD_TIMEOUT)
+            {
+                if(BSP_SD_GetCardState() == SD_TRANSFER_OK)
+                {
+                    res = RES_OK;
+#if defined(ENABLE_SD_DMA_CACHE_MAINTENANCE)
+                    /* The SCB_InvalidateDCache_by_Addr() requires a 32-Byte aligned address,
+                     * adjust the address and the D-Cache size to invalidate accordingly.
+                    */
+                    alignedAddr = (uint32_t)buff & ~0x1F;
+                    SCB_InvalidateDCache_by_Addr((uint32_t*)alignedAddr, count*BLOCKSIZE + ((uint32_t)buff - alignedAddr));
+#endif /* SD_DMA_CACHE_MAINTENANCE */
+                    break;
+                }
+            }
+        }
     }
-    res = RES_OK;
-  }
-
-  return res;
+    
+    return res;
+    
+#else
+    /* Use SD Driver in blocking mode */
+    res = RES_ERROR;
+    if(BSP_SD_ReadBlocks((uint32_t*)buff,
+                         (uint32_t)(sector),
+                         count, SDMMC_HAL_TIMEOUT) == MSD_OK)
+    {
+        /* Wait until the read operation is finished */
+        timeout = HAL_GetTick();
+        while((HAL_GetTick() - timeout) < SD_TIMEOUT)
+        {
+            if(BSP_SD_GetCardState() == MSD_OK)
+            {
+                return RES_OK;
+            }
+        }
+    }
+    
+    return res;
+#endif /* ENABLE_SD_DMA_DRIVER */
 }
 
-/* USER CODE BEGIN beforeWriteSection */
-/* can be used to modify previous code / undefine following code / add new code */
-/* USER CODE END beforeWriteSection */
+
 /**
   * @brief  Writes Sector(s)
   * @param  lun : not used
@@ -164,26 +216,74 @@ DRESULT SD_read(BYTE lun, BYTE *buff, DWORD sector, UINT count)
 #if _USE_WRITE == 1
 DRESULT SD_write(BYTE lun, const BYTE *buff, DWORD sector, UINT count)
 {
-  DRESULT res = RES_ERROR;
-
-  if(BSP_SD_WriteBlocks((uint32_t*)buff,
-                        (uint32_t)(sector),
-                        count, SDMMC_DATATIMEOUT) == MSD_OK)
-  {
-	/* wait until the Write operation is finished */
-    while(BSP_SD_GetCardState() != MSD_OK)
+    DRESULT  res;
+    uint32_t timeout;
+    
+#if defined(ENABLE_SD_DMA_DRIVER)
+    /* Use SD Driver in DMA mode */
+    
+#if defined(ENABLE_SD_DMA_CACHE_MAINTENANCE)
+    uint32_t alignedAddr;
+    alignedAddr = (uint32_t)buff &  ~0x1F;
+    SCB_CleanDCache_by_Addr((uint32_t*)alignedAddr, count*BLOCKSIZE + ((uint32_t)buff - alignedAddr));
+#endif
+    
+    res = RES_ERROR;
+    WriteStatus = 0;
+    
+    if(BSP_SD_WriteBlocks_DMA((uint32_t*)buff, (uint32_t)(sector), count) == MSD_OK)
     {
+        /* Wait for DMA complete */
+        timeout = HAL_GetTick();
+        while((WriteStatus == 0) && ((HAL_GetTick() - timeout) < SD_TIMEOUT))
+        {
+        }
+        
+        /* In case of a timeout return error */
+        if(WriteStatus == 0)
+        {
+            res = RES_ERROR;
+        }
+        else
+        {
+            WriteStatus = 0;
+            timeout = HAL_GetTick();
+            while((HAL_GetTick() - timeout) < SD_TIMEOUT)
+            {
+                if(BSP_SD_GetCardState() == SD_TRANSFER_OK)
+                {
+                    res = RES_OK;
+                    break;
+                }
+            }
+        }
     }
-    res = RES_OK;
-  }
 
-  return res;
+    return res;
+    
+#else
+    /* Use SD Driver in blocking mode */
+    res = RES_ERROR;
+    if(BSP_SD_WriteBlocks((uint32_t*)buff,
+                          (uint32_t)(sector),
+                          count, SDMMC_HAL_TIMEOUT) == MSD_OK)
+    {
+        /* Wait until the Write operation is finished */
+        timeout = HAL_GetTick();
+        while((HAL_GetTick() - timeout) < SD_TIMEOUT)
+        {
+            if(BSP_SD_GetCardState() == MSD_OK)
+            {
+                return RES_OK;
+            }
+        }
+    }
+    
+    return res;
+#endif /* ENABLE_SD_DMA_DRIVER */
 }
 #endif /* _USE_WRITE == 1 */
 
-/* USER CODE BEGIN beforeIoctlSection */
-/* can be used to modify previous code / undefine following code / add new code */
-/* USER CODE END beforeIoctlSection */
 /**
   * @brief  I/O control operation
   * @param  lun : not used
@@ -197,51 +297,64 @@ DRESULT SD_ioctl(BYTE lun, BYTE cmd, void *buff)
   DRESULT res = RES_ERROR;
   BSP_SD_CardInfo CardInfo;
 
-  if (Stat & STA_NOINIT) return RES_NOTRDY;
-
-  switch (cmd)
+  if(Stat & STA_NOINIT) 
   {
-  /* Make sure that no pending write process */
-  case CTRL_SYNC :
-    res = RES_OK;
-    break;
+      return RES_NOTRDY;
+  }
 
-  /* Get number of sectors on the disk (DWORD) */
-  case GET_SECTOR_COUNT :
-    BSP_SD_GetCardInfo(&CardInfo);
-    *(DWORD*)buff = CardInfo.LogBlockNbr;
-    res = RES_OK;
-    break;
+  switch(cmd)
+  {
+      /* Make sure that no pending write process */
+      case CTRL_SYNC :
+        res = RES_OK;
+        break;
 
-  /* Get R/W sector size (WORD) */
-  case GET_SECTOR_SIZE :
-    BSP_SD_GetCardInfo(&CardInfo);
-    *(WORD*)buff = CardInfo.LogBlockSize;
-    res = RES_OK;
-    break;
+      /* Get number of sectors on the disk (DWORD) */
+      case GET_SECTOR_COUNT :
+        BSP_SD_GetCardInfo(&CardInfo);
+        *(DWORD*)buff = CardInfo.LogBlockNbr;
+        res = RES_OK;
+        break;
 
-  /* Get erase block size in unit of sector (DWORD) */
-  case GET_BLOCK_SIZE :
-    BSP_SD_GetCardInfo(&CardInfo);
-    *(DWORD*)buff = CardInfo.LogBlockSize;
-    res = RES_OK;
-    break;
+      /* Get R/W sector size (WORD) */
+      case GET_SECTOR_SIZE :
+        BSP_SD_GetCardInfo(&CardInfo);
+        *(WORD*)buff = CardInfo.LogBlockSize;
+        res = RES_OK;
+        break;
 
-  default:
-    res = RES_PARERR;
+      /* Get erase block size in unit of sector (DWORD) */
+      case GET_BLOCK_SIZE :
+        BSP_SD_GetCardInfo(&CardInfo);
+        *(DWORD*)buff = CardInfo.LogBlockSize;
+        res = RES_OK;
+        break;
+
+      default:
+        res = RES_PARERR;
   }
 
   return res;
 }
 #endif /* _USE_IOCTL == 1 */
 
-/* USER CODE BEGIN afterIoctlSection */
-/* can be used to modify previous code / undefine following code / add new code */
-/* USER CODE END afterIoctlSection */
 
-/* USER CODE BEGIN lastSection */ 
-/* can be used to modify / undefine previous code or add new code */
-/* USER CODE END lastSection */
+/**
+  * @brief Rx Transfer complete callback
+  * @param hsd: SD handle
+  * @retval None
+  */
+void BSP_SD_ReadCpltCallback(void)
+{
+    ReadStatus = 1;
+}
 
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
-
+/**
+  * @brief Tx Transfer complete callback
+  * @param hsd: SD handle
+  * @retval None
+  */
+void BSP_SD_WriteCpltCallback(void)
+{    
+    WriteStatus = 1;
+}
