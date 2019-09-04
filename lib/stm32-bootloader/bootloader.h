e30c73f5f86c9d36e2d3ba23acbacc1321814b50
Akos Pasztor <akos.pasztor@sensirion.com>
2019-09-04 14:22:22 +0200

    Add doxygen documentation to code

--- lib/stm32-bootloader/bootloader.h
+++ lib/stm32-bootloader/bootloader.h
@@ -1,6 +1,6 @@
 /**
   ******************************************************************************
-  * STM32 Bootloader Implementation Header
+  * STM32 Bootloader Header
   ******************************************************************************
   * @author Akos Pasztor
   * @file   bootloader.h
@@ -15,24 +15,45 @@
 #ifndef __BOOTLOADER_H
 #define __BOOTLOADER_H
 
-/*** Bootloader Configuration *************************************************/
-/* Select target MCU family: please define the target MCU family type below.
-   Currently supported MCU families:
-    - STM32L4
+/** Bootloader Configuration
+  * @defgroup Bootloader_Configuration
+  * @{
+  */
+
+/** Select target MCU family: please define the target MCU family type below.
+  * Currently supported MCU families:
+  *  - STM32L4
 */
 #define STM32L4
 
-#define USE_CHECKSUM            0   /* Check application checksum on startup */
-#define USE_WRITE_PROTECTION    0   /* Enable write protection after performing in-app-programming */
-#define SET_VECTOR_TABLE        1   /* Automatically set vector table location before launching application */
-#define CLEAR_RESET_FLAGS       1   /* If enabled: bootloader clears reset flags. (This occurs only when OBL RST flag is active.)
-                                       If disabled: bootloader does not clear reset flags, not even when OBL RST is active. */
+/** Check application checksum on startup */
+#define USE_CHECKSUM            0
 
-#define APP_ADDRESS     (uint32_t)0x08008000    /* Start address of application space in flash */
-#define END_ADDRESS     (uint32_t)0x080FFFFB    /* End address of application space (addr. of last byte) */
-#define CRC_ADDRESS     (uint32_t)0x080FFFFC    /* Start address of application checksum in flash */
-#define SYSMEM_ADDRESS  (uint32_t)0x1FFF0000    /* Address of System Memory (ST Bootloader) */
-/******************************************************************************/
+/** Enable write protection after performing in-app-programming */
+#define USE_WRITE_PROTECTION    0
+
+/** Automatically set vector table location before launching application */
+#define SET_VECTOR_TABLE        1
+
+/** Clear reset flags
+  *  - If enabled: bootloader clears reset flags. (This occurs only when OBL RST flag is active.)
+  *  - If disabled: bootloader does not clear reset flags, not even when OBL RST is active.
+*/
+#define CLEAR_RESET_FLAGS       1
+
+/** Start address of application space in flash */
+#define APP_ADDRESS     (uint32_t)0x08008000
+
+/** End address of application space (address of last byte) */
+#define END_ADDRESS     (uint32_t)0x080FFFFB
+
+/** Start address of application checksum in flash */
+#define CRC_ADDRESS     (uint32_t)0x080FFFFC
+
+/** Address of System Memory (ST Bootloader) */
+#define SYSMEM_ADDRESS  (uint32_t)0x1FFF0000
+/** @} */
+/* End of configuration ------------------------------------------------------*/
 
 /* Includes ------------------------------------------------------------------*/
 /* Include the appropriate header file */
@@ -43,43 +64,45 @@
 #endif
 
 /* Defines -------------------------------------------------------------------*/
-/* Size of application in DWORD (32bits or 4bytes) */
+/** Size of application in DWORD (32bits or 4bytes) */
 #define APP_SIZE                (uint32_t)(((END_ADDRESS - APP_ADDRESS) + 3) / 4)
-/* Number of pages per bank in flash */
+
+/** Number of pages per bank in flash */
 #define FLASH_PAGE_NBPERBANK    (256)
+
 /* MCU RAM information (to check whether flash contains valid application) */
-#define RAM_BASE                SRAM1_BASE
-#define RAM_SIZE                SRAM1_SIZE_MAX
+#define RAM_BASE                SRAM1_BASE          /*!< Start address of RAM */
+#define RAM_SIZE                SRAM1_SIZE_MAX      /*!< RAM size in bytes */
 
 /* Enumerations --------------------------------------------------------------*/
-/*! Bootloader error codes */
+/** Bootloader error codes */
 enum eBootloaderErrorCodes
 {
     BL_OK = 0,          /*!< No error */
-    BL_NO_APP,          /*!< No app TODO */
-    BL_SIZE_ERROR,      /*!< Size error TODO */
-    BL_CHKS_ERROR,      /*!< Chks error TODO */
-    BL_ERASE_ERROR,     /*!< Erase error TODO */
-    BL_WRITE_ERROR,     /*!< Write error TODO */
-    BL_OBP_ERROR        /*!< OBP error TODO */
+    BL_NO_APP,          /*!< No application found in flash */
+    BL_SIZE_ERROR,      /*!< New application is too large for flash */
+    BL_CHKS_ERROR,      /*!< Application checksum error */
+    BL_ERASE_ERROR,     /*!< Flash erase error */
+    BL_WRITE_ERROR,     /*!< Flash write error */
+    BL_OBP_ERROR        /*!< Flash option bytes programming error */
 };
 
-/*! Flash Protection Types */
+/** Flash Protection Types */
 enum eFlashProtectionTypes
 {
-    BL_PROTECTION_NONE  = 0,
-    BL_PROTECTION_WRP   = 0x1,
-    BL_PROTECTION_RDP   = 0x2,
-    BL_PROTECTION_PCROP = 0x4,
+    BL_PROTECTION_NONE  = 0,    /*!< No flash protection */
+    BL_PROTECTION_WRP   = 0x1,  /*!< Flash write protection */
+    BL_PROTECTION_RDP   = 0x2,  /*!< Flash read protection */
+    BL_PROTECTION_PCROP = 0x4,  /*!< Flash propietary code readout protection */
 };
 
 /* Functions -----------------------------------------------------------------*/
-void    Bootloader_Init(void);
+uint8_t Bootloader_Init(void);
 uint8_t Bootloader_Erase(void);
 
-void    Bootloader_FlashBegin(void);
+uint8_t Bootloader_FlashBegin(void);
 uint8_t Bootloader_FlashNext(uint64_t data);
-void    Bootloader_FlashEnd(void);
+uint8_t Bootloader_FlashEnd(void);
 
 uint8_t Bootloader_GetProtectionStatus(void);
 uint8_t Bootloader_ConfigProtection(uint32_t protection);
